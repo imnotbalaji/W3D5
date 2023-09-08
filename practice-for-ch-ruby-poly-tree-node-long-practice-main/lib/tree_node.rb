a4c25527d0b6ad43c2dbd641bf8a05e0c79074bd
Neel <nkachalia1@gmail.com>
2023-09-08 12:10:59 -0400

    Completing add_child and parent methods

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -2,17 +2,48 @@ class PolyTreeNode
     attr_reader :value, :parent, :children
     def initialize(value)
     @value = value
-    @parent = nil 
+    @parent = nil
     @children = []
-  end 
+  end
 
   def parent=(node)
-    if !node
+    if node == nil
+      @parent = node
       return nil
-    end 
+    end
+
+    if @parent != nil && !node.children.include?(self)
+      old_parent = @parent
+      @parent = node
+      node.children << self
+      old_parent.remove_child(self)
+      return self
+    end
+
     if !node.children.include?(self)
       @parent = node
       node.children << self
-    end 
-  end 
+    end
+
+  end
+
+  def add_child(node)
+    if !@children.include?(node)
+      node.parent = self
+    end
+  end
+
+  def remove_child(node)
+    if @children.include?(node)
+      node.parent = nil
+      @children.delete_if(node)
+    else
+      raise 'Node is not a child'
+    end
+  end
+
+  def inspect
+    self.value
+  end
+
 end
